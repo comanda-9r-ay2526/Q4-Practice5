bed3da2756d5c45591ba8690c4dee7124a03d1b4
comanda-9r-ay2526 <comanda.pb@obmontessori.edu.ph>
2026-04-17 13:25:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function freeRobux() {
+    let password = "";
+
+    do {
+        password = prompt("Enter a password longer than 4 characters", "");
+    } while (password.length < 5);
+
+    window.alert("Entered password: " + password);
+}
